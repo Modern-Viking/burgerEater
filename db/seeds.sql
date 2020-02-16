9c471a4f871785f09f11a2447e9652eea576b2bd
Modern Viking <thomascustoms715@gmail.com>
2020-02-16 02:27:44 -0700

    basic file structure and sql files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (name) VALUES ('Luger Burger');
+INSERT INTO burgers (name) VALUES ('Le Pigeon');
+INSERT INTO burgers (name) VALUES ('Double Animal Style');
+INSERT INTO burgers (name) VALUES ('Whiskey King');
+INSERT INTO burgers (name) VALUES ('Western Bacon');
+INSERT INTO burgers (name) VALUES ('Cease and Desist Burger');
